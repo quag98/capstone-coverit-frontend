b46b8ab71f3338d59bde3986e03bfc5b638d0eba
David Quaggin-Smith <quagginsmith@gmail.com>
2021-04-30 11:43:32 -0500

    baseline

--- src/views/SongsIndex.vue
+++ src/views/SongsIndex.vue
@@ -5,6 +5,8 @@
         <h3>{{ song.title }}</h3>
         <p>{{ song.description }}</p>
         <router-link v-bind:to="`/songs/${song.id}`">More Info</router-link>
+        <br>
+        <button v-on:click="createFavorites">Add to Favorites</button>
       </div>
     </div>
   </div>
@@ -16,6 +18,12 @@ export default {
   data: function () {
     return {
       songs: [],
+      title: "",
+      description: "",
+      // audio_link: "",
+      song_id: "",
+      artist_id: "",
+      errors: [],
     };
   },
   created: function () {
@@ -27,6 +35,20 @@ export default {
         this.songs = response.data;
         console.log("all songs:", this.songs);
       });
+    },
+    createFavorites: function () {
+      console.log("adding favorite:");
+      var params = {
+        song_id: this.title,
+        artist_id: this.description,
+        // audio_link: this.audio_link,
+      };
+      axios
+      .post("/api/favorites", params)
+      .then(() => {
+        this.$router.push("/favorites");
+      })
+      .catch((error) => console.log(error.response));
     },
   },
 };
